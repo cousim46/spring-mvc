04051faf8dd78fa3d152296802a5127447588635
cousim46 <wjdghldns24@naver.com>
2021-11-25 22:08:37 +0900

    jsp를 이용한 회원가입, 회원목록보기

--- src/main/java/hello/servlet/domain/member/Member.java
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -13,6 +13,7 @@ public class Member {
     private String username;
     private int age;
 
+
     public Member(String username, int age) {
         this.username = username;
         this.age = age;
